8fdfe5a4422046fa8cd25b258265577a48feab1f
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2017-10-24 17:09:32 +0200

    change sqlite to sqlalchemy and model structure

--- src/db/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists users (
-    id integer primary key autoincrement,
-    username text not null,
-    hash text not null
-    -- salt text not null
-);
--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, session, redirect, url_for, render_template, flash
-from .db.database import Database
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
+app.config.update(dict(SQLALCHEMY_DATABASE_URI='sqlite:////pytodo/db/db.sqlite3'))
+db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
 app.config.update(dict(SECRET_KEY='dev_key'))
 
-database = Database(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(32), nullable=False)
 
 @app.cli.command('initdb')
 def initdb_command():
-    database.init_db()
+    db.create_all(app=app)
     print('Initialized the database.')
 
 @app.route('/test')
@@ -26,19 +33,17 @@ def register_user():
     error = None
 
     if request.method == 'POST':
-        entries = database.query_db('select username from users')
-        for entry in entries:
-            if request.form['user'] == entry['username']:
-                flash('User already taken')
-                return render_template('register.html', error=error)
-    
-        db = database.get_db()
-        db.execute('insert into users (username, hash) values (?, ?)',[request.form['user'], request.form['passwd']])
-        db.commit()
-        flash("Registration was succesfully completed!")
-        return redirect(url_for('login', entries=entries))
+        user = User(username=request.form['user'], password=request.form['passwd'])
+        db.session.add(user)
+        try: 
+            db.session.commit()
+        except IntegrityError:
+            error='Username already taken!'
+            return render_template('register.html', error=error)
+        flash('Registration succesull!')
+        return redirect(url_for('login'))
 
-    return render_template('register.html', error=error)
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,18 +54,21 @@ def login():
         return redirect(url_for('home', error=error))
 
     if request.method == 'POST':
-        match = database.query_db('select * from users where username=?',[request.form['user']])
-        if not match:
-            error = 'Invalid username'
-        else:
-            for entry in match:
-                if entry[2] != request.form['passwd']:
-                    error = 'Invalid password'
-                else:
-                    session['logged_in']=True
-                    session['username']=request.form['user']
-                    flash('You were succesfully logged in!')
-                    return redirect(url_for('home'))
+        try:
+            match = User.query.filter_by(username=request.form['user']).one()
+        except NoResultFound:
+            error = 'Invalid Username'
+            
+        match = User.query.filter_by(username=request.form['user'])
+        for entry in match:
+            if request.form['passwd'] != entry.password:
+                error = 'Invalid password'
+            else:
+                session['logged_in']=True
+                session['username']=request.form['user']
+                flash('You were succesfully logged in!')
+                return redirect(url_for('home'))
+
     return render_template('login.html', error=error)
 
 @app.route('/logout')
