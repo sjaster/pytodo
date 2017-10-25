9ac76a6703d35b3f9cea8c0b516f072056ce2fbd
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2017-10-25 20:07:56 +0200

    add model for subject

--- src/models.py
+++ src/models.py
@@ -12,12 +12,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(32), nullable=False)
-    cards = db.relationship('Card', backref='user')
+    subjects = db.relationship('Subject', backref='user', lazy=True)
+    cards = db.relationship('Card', backref='user', lazy=True)
+
 
 class Card(db.Model):
     __tablename__ = 'card'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(32))
-    content = db.Column(db.Text())
+    title = db.Column(db.String(32), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
     state = db.Column(db.Enum(State))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
+
+class Subject(db.Model):
+    __tablename__ = 'subjects'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    cards = db.relationship('Card', backref='subjects', lazy=True)
